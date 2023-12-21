94c65dc7b42376b9925e4a543158678d586c18eb
Demurest <eric.apolonio@hotmail.com>
2023-12-21 20:00:25 -0300

    V 1.0

--- SRC/Controllers/clienteController.js
+++ SRC/Controllers/clienteController.js
@@ -97,3 +97,83 @@ exports.getClientsFollowUp = async (ctx) => {
         ctx.body = { error: err.message };
     }
 }
+
+exports.getCalculatedClient = async (ctx) =>{
+    /**El propósito de este método es determinar si un cliente puede acceder a la compra de un producto. Se realiza asignando valores entre 0 y 25 a los siguientes aspectos:
+
+    1. Cantidad de mensajes.
+    2. Ahorros acumulados hasta la fecha.
+    3. Monto de deudas.
+    4. Salario del cliente.
+
+    Una vez asignados los valores respectivos a cada aspecto, se suman para obtener un total que determinará la viabilidad de la compra.
+     */
+
+    try{
+        let pesoMessage = 0
+        let pesoAhorro = 0
+        let pesoAmount = 0
+        let pesoSalario = 0
+
+        const clientId = ctx.params.clientId
+        const client = await Client.findOne({
+            where: { id: clientId },
+            include: [
+                { model: Deuda },
+                { model: Message }
+            ]
+        })
+
+        /*para medir el peso de los mensajes*/
+        if(client.messages.length < 1){
+            pesoMessage = 0
+        }
+        if(client.messages.length > 5){
+            pesoMessage = 10
+        }
+        if(client.messages.length > 10){
+            pesoMessage = 25
+        }
+
+        /*para medir el peso de los ahorros*/
+        if(client.ahorros < 1000000){
+            pesoAhorro = 0
+        }
+        if(client.ahorros > 5000000){
+            pesoAhorro = 10
+        }
+        if(client.ahorros > 10000000){
+            pesoAhorro = 25
+        }
+
+        /**para medir el peso de las deudas */
+        if(client.deudas.amount < 1000000){
+            pesoAmount = 25
+        }
+        if(client.deudas.amount > 5000000){
+            pesoAmount = 10
+        }
+        if(client.deudas.amount > 10000000){
+            pesoAmount = 0
+        }
+
+        /**para medir el peso del salario */
+        if(client.salario < 1000000){
+            pesoSalario = 0
+        }
+        if(client.salario > 1000000 && client.salario < 1500000){
+            pesoSalario = 10
+        }
+        if(client.salario > 1500000){
+            pesoSalario = 25
+        }
+
+        const resultadoFinal = pesoMessage + pesoAhorro + pesoAmount + pesoSalario
+
+        ctx.status = 200
+        ctx.body = { "score": resultadoFinal}
+    } catch (err){
+        ctx.status = 500;
+        ctx.body = { error: err.message };
+    }
+}
--- SRC/routes/clientRoutes.js
+++ SRC/routes/clientRoutes.js
@@ -7,5 +7,6 @@ router.get('/',clientController.getClients);
 router.post('/',clientController.createClient);
 router.get('/clients-to-do-follow-up',clientController.getClientsFollowUp);
 router.get('/:clientId',clientController.getClientInfo);
+router.get('/:clientId/score',clientController.getCalculatedClient);
 
 module.exports = router;
